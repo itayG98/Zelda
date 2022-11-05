ad2571327b16c6e4a5b5e7ef8f6a55d2e0bca52a
GEITAY5@365.openu.ac.il <GEITAY5@365.openu.ac.il>
2022-11-05 12:54:10 +0200

    Added migration and started picking Ice Cream Component

---
+++ Zelda/Migrations/20221105101114_updateWeightInOrderModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zelda.Migrations
+{
+    public partial class updateWeightInOrderModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IceCreamWeight",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IceCreamWeight",
+                table: "Orders");
+        }
+    }
+}
